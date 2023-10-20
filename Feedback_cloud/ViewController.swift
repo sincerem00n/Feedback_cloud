a33f0e93e1911e358937956bfd3f704d88524675
win <winnie>
2023-10-20 23:07:22 +0700

    Initial Commit

---
+++ Feedback_cloud/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Feedback_cloud
+//
+//  Created by Phattawin Kummaraphat on 20/10/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Feedback_cloudUITests/Feedback_cloudUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Feedback_cloudUITestsLaunchTests.swift
+//  Feedback_cloudUITests
+//
+//  Created by Phattawin Kummaraphat on 20/10/2566 BE.
+//
+
+import XCTest
+
+final class Feedback_cloudUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
